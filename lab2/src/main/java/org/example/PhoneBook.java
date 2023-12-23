3a56bcfd7852bdc08a02faa1ad2597c72c659ce6
kemper0110 <danil2003.2043@gmail.com>
2023-12-23 23:16:28 +0300

    optimize imports

--- lab2/src/main/java/org/example/PhoneBook.java
+++ lab2/src/main/java/org/example/PhoneBook.java
@@ -1,18 +1,9 @@
 package org.example;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 import java.util.ArrayList;
-import java.util.Spliterator;
-import java.util.Spliterators;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
-
-import static java.util.Spliterator.ORDERED;
 
 public class PhoneBook {
     @AllArgsConstructor
