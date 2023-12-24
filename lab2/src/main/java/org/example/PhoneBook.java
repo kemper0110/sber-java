4a5a123d6ff233d4366e0ede661ce0a6404cd407
kemper0110 <danil2003.2043@gmail.com>
2023-12-24 14:47:12 +0300

    add stream support

--- lab2/src/main/java/org/example/PhoneBook.java
+++ lab2/src/main/java/org/example/PhoneBook.java
@@ -1,7 +1,14 @@
 package org.example;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Spliterators;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static java.util.Spliterator.ORDERED;
 
 public class PhoneBook {
     record PhoneNode(String phone, PhoneNode next) { }
@@ -9,7 +16,19 @@ public class PhoneBook {
     public void add(String surname, String phone) {
         map.put(surname, new PhoneNode(phone, map.get(surname)));
     }
-    public PhoneNode get(String surname) {
-        return map.get(surname);
+    public Stream<String> get(String surname) {
+        final var list = map.get(surname);
+        final var spliterator = new Spliterators.AbstractSpliterator<String>(Long.MAX_VALUE, ORDERED){
+            PhoneNode head = list;
+            @Override
+            public boolean tryAdvance(Consumer<? super String> action) {
+                if (head == null)
+                    return false;
+                action.accept(head.phone);
+                head = head.next;
+                return true;
+            }
+        };
+        return StreamSupport.stream(spliterator, false);
     }
 }
