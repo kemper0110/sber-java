edc9ff29e4b1e65fda6248c06f4e3cebef4d30ca
kemper0110 <danil2003.2043@gmail.com>
2024-01-05 17:35:52 +0300

    + tests

--- lab4/src/test/java/org/example/models/PinValidatorTest.java
+++ lab4/src/test/java/org/example/models/PinValidatorTest.java
@@ -1,20 +1,50 @@
 package org.example.models;
 
+import org.example.exceptions.pin.PinDigitsException;
+import org.example.exceptions.pin.PinLengthException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PinValidatorTest {
+    @Test
+    void validateExcessSize() {
+        final var validator = new PinValidator();
+        final var pins = new String[]{
+                "1234233",
+                "12344",
+                "141244",
+        };
+        for (String pin : pins)
+            assertThrowsExactly(PinLengthException.class, () -> validator.validate(pin));
+    }
+    @Test
+    void validateNotDigits() {
+        final var validator = new PinValidator();
+        final var pins = new String[]{
+                "abob",
+                "12a",
+                "123a",
+                "12a2",
+                "a223",
+        };
+        for (String pin : pins)
+            assertThrowsExactly(PinDigitsException.class, () -> validator.validate(pin));
+    }
 
     @Test
-    void validate() {
-//        final var validator = new PinValidator();
+    void validateLess4Digits() {
+        final var validator = new PinValidator();
 
-//        assertDoesNotThrow(() -> validator.validate("1234"));
-//        assertDoesNotThrow(() -> validator.validate("123"));
-//        assertDoesNotThrow(() -> validator.validate("1"));
-//        assertDoesNotThrow(() -> validator.validate("12"));
-//        assertDoesNotThrow(() -> validator.validate("223"));
+        final var pins = new String[]{
+                "1234",
+                "123",
+                "1",
+                "12",
+                "223",
+        };
 
+        for (String pin : pins)
+            assertDoesNotThrow(() -> validator.validate(pin));
     }
 }
