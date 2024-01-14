d94903990e91de71da8f447aaeb42da8adc86bb6
Danil <danil2003.2043@gmail.com>
2024-01-14 21:18:56 +0300

    update pin validator tests

--- lab4/src/test/java/org/example/models/PinValidatorTest.java
+++ lab4/src/test/java/org/example/models/PinValidatorTest.java
@@ -2,6 +2,8 @@ package org.example.models;
 
 import org.example.exceptions.pin.PinDigitsException;
 import org.example.exceptions.pin.PinLengthException;
+import org.example.exceptions.pin.PinMaxLengthException;
+import org.example.exceptions.pin.PinMinLengthException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,35 +18,32 @@ class PinValidatorTest {
                 "141244",
         };
         for (String pin : pins)
-            assertThrowsExactly(PinLengthException.class, () -> validator.validate(pin));
+            assertThrowsExactly(PinMaxLengthException.class, () -> validator.validate(pin));
+    }
+    @Test
+    void validateLess4Digits() {
+        final var validator = new PinValidator();
+        final var pins = new String[]{
+                "123",
+                "123",
+                "1",
+                "12",
+                "223",
+        };
+        for (String pin : pins)
+            assertThrowsExactly(PinMinLengthException.class, () -> validator.validate(pin));
     }
     @Test
     void validateNotDigits() {
         final var validator = new PinValidator();
         final var pins = new String[]{
                 "abob",
-                "12a",
+                "12aa",
                 "123a",
                 "12a2",
                 "a223",
         };
         for (String pin : pins)
             assertThrowsExactly(PinDigitsException.class, () -> validator.validate(pin));
-    }
-
-    @Test
-    void validateLess4Digits() {
-        final var validator = new PinValidator();
-
-        final var pins = new String[]{
-                "1234",
-                "123",
-                "1",
-                "12",
-                "223",
-        };
-
-        for (String pin : pins)
-            assertDoesNotThrow(() -> validator.validate(pin));
     }
 }
