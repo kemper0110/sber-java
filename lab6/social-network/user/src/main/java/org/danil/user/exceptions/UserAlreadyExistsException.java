4ef10a617ad9f4e92fa2b674a8b382bd8960157e
Danil <danil2002.2043@gmail.com>
2024-01-20 19:37:30 +0300

    update user exceptions

--- lab6/social-network/user/src/main/java/org/danil/user/exceptions/UserAlreadyExistsException.java
+++ lab6/social-network/user/src/main/java/org/danil/user/exceptions/UserAlreadyExistsException.java
@@ -1,4 +1,22 @@
 package org.danil.user.exceptions;
 
 public class UserAlreadyExistsException extends UserException {
+    public UserAlreadyExistsException() {
+    }
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserAlreadyExistsException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserAlreadyExistsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
