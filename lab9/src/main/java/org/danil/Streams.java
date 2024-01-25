b842b537cd22f5de611d9178bdda288112480e52
Danil <danil2002.2043@gmail.com>
2024-01-25 17:57:33 +0300

    done

--- lab9/src/main/java/org/danil/Streams.java
+++ lab9/src/main/java/org/danil/Streams.java
@@ -8,42 +8,46 @@ import java.util.function.Predicate;
 import static java.util.Spliterator.ORDERED;
 
 public class Streams<T> {
-    Spliterator<T> spliterator;
+    Iterator<T> iterator;
 
-    Streams(Spliterator<T> spliterator) {
-        this.spliterator = spliterator;
+    Streams(Iterator<T> iterator) {
+        this.iterator = iterator;
     }
 
     public static <T> Streams<T> of(Iterable<T> list) {
-        return new Streams<>(list.spliterator());
+        return new Streams<>(list.iterator());
     }
 
     public Streams<T> filter(Predicate<? super T> predicate) {
-        return new Streams<>(new Spliterators.AbstractSpliterator<T>(Long.MAX_VALUE, ORDERED) {
+
+        return new Streams<>(new Iterator<T>() {
             T value = null;
             @Override
-            public boolean tryAdvance(Consumer<? super T> action) {
-                while(spliterator.tryAdvance(value -> this.value = value)) {
-                    if(!predicate.test(value)) continue;
-
-                    action.accept(value);
-                    return true;
+            public boolean hasNext() {
+                while(iterator.hasNext()) {
+                    value = iterator.next();
+                    if(predicate.test(value))
+                        return true;
                 }
                 return false;
+            }
+            @Override
+            public T next() {
+                return value;
             }
         });
     }
 
     public <V> Streams<V> transform(Function<? super T, ? extends V> mapper) {
-        return new Streams<>(new Spliterators.AbstractSpliterator<V>(Long.MAX_VALUE, ORDERED) {
-            T value = null;
+        return new Streams<>(new Iterator<V>() {
             @Override
-            public boolean tryAdvance(Consumer<? super V> action) {
-                if(!spliterator.tryAdvance(value -> this.value = value))
-                    return false;
+            public boolean hasNext() {
+                return iterator.hasNext();
+            }
 
-                action.accept(mapper.apply(value));
-                return true;
+            @Override
+            public V next() {
+                return mapper.apply(iterator.next());
             }
         });
     }
@@ -51,9 +55,9 @@ public class Streams<T> {
     public <K, V> Map<K, V> toMap(Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends V> valueMapper) {
         final var map = new HashMap<K, V>();
 
-        while(spliterator.tryAdvance(t -> map.put(keyMapper.apply(t), valueMapper.apply(t)))) {
-
-        }
+        iterator.forEachRemaining(t ->
+                map.put(keyMapper.apply(t), valueMapper.apply(t))
+        );
 
         return map;
     }
