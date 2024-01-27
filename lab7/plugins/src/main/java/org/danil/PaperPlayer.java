825bf88d6a06358aa8338d4df9910246e1b6b9ab
Danil <danil2002.2043@gmail.com>
2024-01-27 20:32:29 +0300

    managed modules deps

---
+++ lab7/plugins/src/main/java/org/danil/PaperPlayer.java
@@ -0,0 +1,8 @@
+package org.danil;
+
+public class PaperPlayer implements RockPaperScissorsPlayer{
+    @Override
+    public RockPaperScissorsEnum play() {
+        return RockPaperScissorsEnum.PAPER;
+    }
+}
