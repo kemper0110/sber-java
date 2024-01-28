232c4f31fc56920458eb112664bf98014dbeea97
Danil <danil2002.2043@gmail.com>
2024-01-28 19:17:31 +0300

    console input for browser

--- lab7/browser/src/main/java/org/danil/Main.java
+++ lab7/browser/src/main/java/org/danil/Main.java
@@ -1,9 +1,19 @@
 package org.danil;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        final var pluginManager = new PluginManager("file:///C:/Users/Danil/IdeaProjects/sber-java/lab7/plugins");
+        final var scanner = new Scanner(System.in);
+
+        System.out.print("Root directory(default=my): ");
+
+        final var defaultDirectory = "file:///C:/Users/Danil/IdeaProjects/sber-java/lab7/plugins";
+        final var inputDirectory = scanner.nextLine();
+        final var rootDirectory = inputDirectory == "" ? defaultDirectory : inputDirectory;
+
+        final var pluginManager = new PluginManager(rootDirectory);
 
         final var basicPlugin = pluginManager.load("BasicPlugin/target/BasicPlugin-1.0.jar", "org.danil.BasicPlugin");
         basicPlugin.doUsefull();
