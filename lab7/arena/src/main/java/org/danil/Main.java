71b55f8c8a22634fcf2252750c8218101e9755f2
Danil <danil2002.2043@gmail.com>
2024-01-28 19:15:01 +0300

    migrate game to jars

--- lab7/arena/src/main/java/org/danil/Main.java
+++ lab7/arena/src/main/java/org/danil/Main.java
@@ -3,12 +3,15 @@ package org.danil;
 import java.util.Scanner;
 
 public class Main {
+
+    record PluginData(String path, String name) {}
+
     public static void main(String[] args) {
         final var scanner = new Scanner(System.in);
 
         System.out.print("Root directory(default=my): ");
 
-        final var defaultDirectory = "file:///C:/Users/Danil/IdeaProjects/sber-java/lab7/players/target/players-1.0.jar";
+        final var defaultDirectory = "file:///C:/Users/Danil/IdeaProjects/sber-java/lab7/players";
         final var inputDirectory = scanner.nextLine();
         final var rootDirectory = inputDirectory == "" ? defaultDirectory : inputDirectory;
 
@@ -22,20 +25,20 @@ public class Main {
 
         final var playerManager = new PlayerManager(rootDirectory);
 
-        final var plugins = new String[]{
-                "org.danil.PaperPlayer",
-                "org.danil.RandomPlayer",
-                "org.danil.RockHaterPlayer"
+        final var plugins = new PluginData[]{
+                new PluginData("PaperPlayer/target/PaperPlayer-1.0.jar", "org.danil.PaperPlayer"),
+                new PluginData("RandomPlayer/target/RandomPlayer-1.0.jar", "org.danil.RandomPlayer"),
+                new PluginData("RockHaterPlayer/target/RockHaterPlayer-1.0.jar", "org.danil.RockHaterPlayer"),
         };
 
 
         for(int i = 0; i < iterationsNumber; ++i) {
             System.out.println("\n\n\t\t\tITERATION " + i);
 
-            var bestPlayer = playerManager.load(plugins[0]);
+            var bestPlayer = playerManager.load(plugins[0].path(), plugins[0].name());
 
             for (int j = 1; j < plugins.length; ++j) {
-                var oppositePlayer = playerManager.load(plugins[j]);
+                var oppositePlayer = playerManager.load(plugins[j].path(), plugins[j].name());
 
                 System.out.println("Playing " + bestPlayer.getClass().getName() + " vs " + oppositePlayer.getClass().getName());
 
--- lab7/arena/src/main/java/org/danil/PlayerManager.java
+++ lab7/arena/src/main/java/org/danil/PlayerManager.java
@@ -9,9 +9,9 @@ import java.net.URLClassLoader;
 public class PlayerManager {
     private final String pluginRootDirectory;
 
-    public RockPaperScissorsPlayer load(String pluginClassName) {
+    public RockPaperScissorsPlayer load(String pluginPath, String pluginClassName) {
         try {
-            final var classloader = new URLClassLoader(new URL[]{new URL(pluginRootDirectory)});
+            final var classloader = new URLClassLoader(new URL[]{new URL(pluginRootDirectory + "/" + pluginPath)});
             return (RockPaperScissorsPlayer) classloader.loadClass(pluginClassName).getConstructor().newInstance();
         } catch (Exception e) {
             throw new RuntimeException(e);
