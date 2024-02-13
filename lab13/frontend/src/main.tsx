eae810d036fb898a5a9648e05960745f09c8ff03
Danil <danil2002.2043@gmail.com>
2024-02-13 18:56:44 +0300

    + frontend

---
+++ lab13/frontend/src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import '@mantine/core/styles.css';
+import { createTheme, MantineProvider } from '@mantine/core';
+import '@mantine/charts/styles.css';
+import '@mantine/spotlight/styles.css';
+
+const theme = createTheme({
+    /** Put your mantine theme override here */
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <MantineProvider theme={theme}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </MantineProvider>,
+)
