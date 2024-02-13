592e6a60a347be495e71901ef5cf292376665d54
Danil <danil2002.2043@gmail.com>
2024-02-13 20:58:45 +0300

    rm old

--- lab13/src/main/java/org/danil/lab13/repository/ResponseRepository.java
+++
@@ -1,9 +0,0 @@
-package org.danil.lab13.repository;
-
-import org.danil.lab13.model.Response;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResponseRepository extends ReactiveCrudRepository<Response, Long> {
-}
