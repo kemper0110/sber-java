5a7599dcd8fe209da538105ce1840c0693c4dcb1
Danil <danil2002.2043@gmail.com>
2024-02-17 17:16:17 +0300

    add exception handling

--- lab14/src/main/java/org/danil/Task.java
+++ lab14/src/main/java/org/danil/Task.java
@@ -3,7 +3,11 @@ package org.danil;
 import java.util.concurrent.Callable;
 
 public class Task<T> {
-    volatile T result;
+    enum Status {EMPTY, RESULT, EXCEPTION}
+
+    T result;
+    TaskException exception;
+    volatile Status status = Status.EMPTY;
     Callable<? extends T> callable;
 
     public Task(Callable<? extends T> callable) {
@@ -11,18 +15,21 @@ public class Task<T> {
     }
 
     public T get() throws TaskException {
-        if (result != null)
-            return result;
-
-        synchronized (this) {
-            if (result == null) {
-                try {
-                    result = callable.call();
-                } catch (Exception ex) {
-                    throw new TaskException(ex);
+        if (status == Status.EMPTY) {
+            synchronized (this) {
+                if (status == Status.EMPTY) {
+                    try {
+                        result = callable.call();
+                        status = Status.RESULT;
+                    } catch (Exception ex) {
+                        exception = new TaskException(ex);
+                        status = Status.EXCEPTION;
+                    }
                 }
             }
         }
+        if (status == Status.EXCEPTION)
+            throw exception;
         return result;
     }
 }
