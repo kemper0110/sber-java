277b07d8cea673018e33eb8a4c2d278787043373
Danil <danil2002.2043@gmail.com>
2024-03-07 17:54:48 +0300

    oops.. Enabling TM

--- lab18/src/main/java/org/danil/lab18/Lab18Application.java
+++ lab18/src/main/java/org/danil/lab18/Lab18Application.java
@@ -2,8 +2,10 @@ package org.danil.lab18;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class Lab18Application {
 
     public static void main(String[] args) {
